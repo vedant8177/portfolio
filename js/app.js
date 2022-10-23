e0cff1646e18831da7b9a74240369122e1eddac1
vedant <vedantsapalkar99@gmail.com>
2022-10-23 10:49:03 +0530

    fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const menu = document.getElementsByClassName("navbar_btn")[0];
 const close = document.getElementsByClassName("hamburger_close")[0];
 const hamburger = document.getElementsByClassName("hamburger")[0];
+const listItem = document.querySelectorAll(".hamburger_list_items");
+
 
 close.addEventListener("click", () => {
     hamburger.style.transform = "translateY(-500px)";
@@ -9,3 +11,9 @@ close.addEventListener("click", () => {
 menu.addEventListener("click", () => {
     hamburger.style.transform = "translateY(0)";
 });
+
+listItem.forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.style.transform = "translateY(-500px)";
+    })
+})
